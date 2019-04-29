8c99bd8925c65dee23a082a614704cd0b02ad9af
kriepy <k.rieping@gmail.com>
2019-04-29 12:23:12 +0200

    implement signin form begin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from './Navigation';
 
 import LandingPage from './Landing';
-import SignInPage from './SignIn';
+import SignInPage from './SignIn/index';
 import SignOutPage from './SignOut';
 import HomePage from './Home';
 
